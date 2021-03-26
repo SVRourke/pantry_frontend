2e1c63f9a4eac344ee0491131675567ee829c663
SVRourke <samuelvanrourke@gmail.com>
2021-03-26 15:55:06 -0400

    Feeling out routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Lists from "./components/Lists";
+import Friends from "./components/Friends";
+import Welcome from "./components/Welcome";
+import Login from './components/Login';
+
 function App() {
   return (
-    <h1>Working</h1>
+    <div>
+      <BrowserRouter>
+        <Link to="/" >home</Link>
+        <Link to="/lists" >Lists</Link>
+        <Link to="/friends" >Friends</Link>
+
+        <Switch>
+          <Route exact path='/' component={Welcome} />
+          <Route path='/lists' component={Lists} />
+          <Route path='/friends' component={Friends} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   )
 }
 
