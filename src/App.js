a443009fd91594ccaca8ad47234d166e1a6d6be8
SVRourke <samuelvanrourke@gmail.com>
2021-03-26 17:43:26 -0400

    refactored

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 
-import Lists from "./components/Lists";
-import Friends from "./components/Friends";
-import Welcome from "./components/Welcome";
-import Login from './components/Login';
+import Lists from './components/Lists'
+import Friends from './components/Friends'
+import Welcome from './components/Welcome'
+import Login from './components/Login'
 
-function App() {
+function App () {
   return (
     <div>
-      <BrowserRouter>
-        <Link to="/" >home</Link>
-        <Link to="/lists" >Lists</Link>
-        <Link to="/friends" >Friends</Link>
-
-        <Switch>
-          <Route exact path='/' component={Welcome} />
-          <Route path='/lists' component={Lists} />
-          <Route path='/friends' component={Friends} />
-          <Route path='/login' component={Login} />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Welcome} />
+        <Route path='/lists' component={Lists} />
+        <Route path='/friends' component={Friends} />
+        <Route path='/login' component={Login} />
+      </Switch>
     </div>
   )
 }
 
-export default App;
+export default App
