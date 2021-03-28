a92ef0cc2b583b0bed6a961b9279aaed9c59430a
SVRourke <samuelvanrourke@gmail.com>
2021-03-28 09:27:36 -0400

    playing around with routes

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { Route, Switch, Link, useRouteMatch } from "react-router-dom"
 
-const Lists = () => {
-    return (
-        <div>
-            <h1>Lists</h1>
-        </div>
-    );
+const Test = () => {
+  return "List"
 }
 
-export default Lists;
+const Lists = () => {
+  const records = [
+    { id: 3, name: 'First' },
+    { id: 4, name: 'Second' },
+    { id: 5, name: 'Third' },
+    { id: 6, name: 'Fourth' },
+    { id: 7, name: 'Fifth' },
+    { id: 8, name: 'Sixth' }
+  ]
+
+  let { path, url } = useRouteMatch();
+
+  const links = records.map(r => <Link to={`${url}/${r.id}`} >{r.name}</Link>)
+  return (
+        <div>
+            <h1>Lists</h1>
+            {links}
+            <Switch>
+              <Route path={`${url}/:id`} component={Test} />
+            </Switch>
+        </div>
+  )
+}
+
+export default Lists
