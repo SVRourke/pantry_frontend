dc0fd9ad4a25a1ca3fdbb03b819a0c073c530798
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 12:36:17 -0400

    added comments

--- src/components/indexes/FriendIndex.js
+++ src/components/indexes/FriendIndex.js
@@ -3,9 +3,19 @@ import React from 'react'
 const FriendIndex = () => {
   return (
     <div>
+      {/* 
+        Component that maps a user's Friends to FriendCard Components and displays them
+      */}
       <h3>Friends List Component</h3>
+      
+      {/*
+        Button Link To new List Page
+      */}
       <h5>Invite Friends Button</h5>
-      <hr/>
+
+      {/* 
+        Tabbed Requests Component filters rendered requests by 'sent' or 'received'  
+      */}
       <h3>Friends Requests component</h3>
     </div>
   )
