991599dd0fd47f7b8597acfbb74ad7ea3b78e38f
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 12:36:51 -0400

    created ListCardContainer and ListInvitedContainer stub components

--- src/components/indexes/ListsIndex.js
+++ src/components/indexes/ListsIndex.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import ListCardContainer from '../ListCardContainer'
+import ListInvitesContainer from '../ListInvitesContainer'
+import { Link } from 'react-router-dom'
+
+import styled from 'styled-components'
+const FancyLink = styled.div`
+  display: block;   
+    a {
+      border: solid black 2px;
+      padding: .5rem 2rem;
+      margin: 2rem 0;
+    }
+`;
+
 
 const ListIndex = () => {
-
   return (
-        <div>
-            <h3>Lists List Component </h3>
-            <h5>New List Button</h5>
-            <hr/>
-            <h3>Invites Component</h3>
-        </div>
+    <div>
+      <ListCardContainer />
+
+      <FancyLink>
+        <Link to={'lists/new'}>New List Button</Link>
+      </FancyLink>
+
+      <ListInvitesContainer />
+    </div>
   )
 }
 
 export default ListIndex
 
-{/* {links}
-<Switch>
-  <Route path={`${url}/:id`} component={Test} />
-</Switch> */}
+// 
