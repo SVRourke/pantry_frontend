b95171d589ec193bec0295db9b8f620cdbb9a186
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 10:47:39 -0400

    modified routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom'
 
 import Lists from './components/Lists'
 import Friends from './components/Friends'
 import Welcome from './components/Welcome'
 import Login from './components/Login'
+import ProtectedContainer from './Routers/ProtectedContainer'
 
 function App () {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Welcome} />
-        <Route path='/lists' component={Lists} />
-        <Route path='/friends' component={Friends} />
         <Route path='/login' component={Login} />
+        {/* <Route path='/about' component={About} /> */}
+        <Route path={`/users/:id`} component={ProtectedContainer} />
       </Switch>
     </div>
   )
