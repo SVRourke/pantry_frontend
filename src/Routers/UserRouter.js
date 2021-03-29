cebfaa805537aba17f6c2564004006c30a3198ff
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 11:33:35 -0400

    refactored UserRouter

---
+++ src/Routers/UserRouter.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  useRouteMatch,
+  useParams 
+} from 'react-router-dom'
+
+import Lists from '../components/indexes/ListsIndex'
+import Friends from '../components/indexes/FriendIndex'
+import Account from '../components/pages/Account'
+
+const UserRouter = () => {
+  const user_id = useParams()
+  const { path, url } = useRouteMatch()
+
+  return (
+    <div>
+      {console.log(url)}
+      <Link to={`${url}/lists`}>lists</Link>
+      <Link to={`${url}/friends`}>friends</Link>
+      <Link to={`${url}/account`}>account</Link>
+
+      <Switch>
+        <Route exact path={`${path}/`} >
+          <Redirect to={`${url}/lists`} />
+        </Route>
+        <Route path={`${path}/lists`} component={Lists} />
+        <Route path={`${path}/friends`} component={Friends} />
+        <Route path={`${path}/account`} component={Account} />
+      </Switch>
+    </div>
+  )
+}
+
+export default UserRouter
