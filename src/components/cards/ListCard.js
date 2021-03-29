94b4ebd32262dc80d76eaa6af5b0176db71c013c
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 15:07:59 -0400

    created list card

---
+++ src/components/cards/ListCard.js
@@ -0,0 +1,49 @@
+
+// ALERT: ADD LATEST ACTIVITY TIME TO LIST INDEX RESPONSE * BACKEND * 
+import React from 'react'
+import styled from 'styled-components'
+import {
+  CardBody,
+  CardHeading,
+  MemberCount,
+  TimeDisplay
+} from './elements'
+
+const ListCard = (props) => {
+  const { id, name, contributor_count, item_count } = props.user
+  return (
+    <div>
+      <CardBody>
+        <CardHeading>{name}</CardHeading>
+        <p>Add most recent to response!</p>
+
+        {/* <TimeDisplay style={{ color: color }}>
+                  activity {latestactivity} ago
+              {/* </TimeDisplay> */}
+
+        <MemberCount>{contributor_count} members</MemberCount>
+      </CardBody>
+
+    </div>
+  )
+}
+
+export default ListCard
+
+// * LOGIC FOR CHANGING THE TEXT Color depending on how much time has passed
+// const timeType = latestactivity.split(" ")[1];
+// let color = "";
+
+// switch (timeType) {
+//     case "minutes":
+//     color = "var(--green-color)";
+//     break;
+//     case "hours":
+//     color = "var(--blue-color)";
+//     break;
+//     case "days":
+//     color = "var(--offBlack-color)";
+//     break;
+//     default:
+//     color = "var(--gray-color)";
+// }
