dad5a2b05302739dce61361ffacf8cfd85f1b7b7
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 11:31:52 -0400

    Renamed Friends FriendIndex

---
+++ src/components/indexes/FriendIndex.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FriendIndex = () => {
+  return (
+    <div>
+      <h3>Friends List Component</h3>
+      <h5>Invite Friends Button</h5>
+      <hr/>
+      <h3>Friends Requests component</h3>
+    </div>
+  )
+}
+
+export default FriendIndex
