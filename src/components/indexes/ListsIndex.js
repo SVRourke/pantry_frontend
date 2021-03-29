3060b1c388f0506aea73637a3c48c0d50faec83b
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 11:32:14 -0400

    renamed Lists ListIndex

---
+++ src/components/indexes/ListsIndex.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ListIndex = () => {
+
+  return (
+        <div>
+            <h3>Lists List Component </h3>
+            <h5>New List Button</h5>
+            <hr/>
+            <h3>Invites Component</h3>
+        </div>
+  )
+}
+
+export default ListIndex
+
+{/* {links}
+<Switch>
+  <Route path={`${url}/:id`} component={Test} />
+</Switch> */}
