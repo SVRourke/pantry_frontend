aa495ecb4c764a0c191243dac4f23536c0a77f40
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 12:35:46 -0400

    created ListCardContainer

---
+++ src/components/ListCardContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components'
+
+const Block = styled.div`
+    width: 100%;
+    height: 60vh;
+    background-color: #CACACA;
+`; 
+
+const ListCardContainer = () => {
+    return (
+        <Block>
+            <h3>Lists List Component </h3>
+        </Block>
+    );
+}
+
+export default ListCardContainer;
