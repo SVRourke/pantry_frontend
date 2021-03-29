ecb3c7b279888411ddc23ef6aceb91c65cb06387
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 12:36:05 -0400

    created ListInvitesContainer

---
+++ src/components/ListInvitesContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components'
+
+const Block = styled.div`
+    width: 100%;
+    height: 20vh;
+    background-color: #CACACA;
+`; 
+
+const ListInvitesContainer = () => {
+    return (
+        <Block>
+            <h3>Invites Component</h3>
+        </Block>
+    );
+}
+
+export default ListInvitesContainer;
