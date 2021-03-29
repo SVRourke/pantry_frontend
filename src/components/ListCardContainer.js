2fdf9b533137e6b417e1a9a065ad51feef32ce45
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 15:07:37 -0400

    formatting

--- src/components/ListCardContainer.js
+++ src/components/ListCardContainer.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from 'react'
+
+import ListCard from './cards/ListCard'
 
 import styled from 'styled-components'
 
 const Block = styled.div`
     width: 100%;
     height: 60vh;
-    background-color: #CACACA;
-`; 
+    overflow-y: scroll;
+    ${'' /* background-color: #CACACA; */}
+`
 
-const ListCardContainer = ({records}) => {
-    const cards = records.map(r => <p>{r.name}</p>)
-    
-    return (
-        <Block>
-            <h3>Lists List Component </h3>
-            {cards}
-        </Block>
-    );
+const ListCardContainer = ({ records }) => {
+  const cards = records.map(r => <ListCard key={r.id} user={r} />)
+
+  return (
+    <Block>
+      <h3>Lists List Component </h3>
+      {cards}
+    </Block>
+  )
 }
 
-export default ListCardContainer;
+export default ListCardContainer
