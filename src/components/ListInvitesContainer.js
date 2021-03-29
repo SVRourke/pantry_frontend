e6d757b1bc727f579bb223c14f67171bbaeafe69
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 17:10:52 -0400

    built out list invites container compnent

--- src/components/ListInvitesContainer.js
+++ src/components/ListInvitesContainer.js
@@ -1,22 +1,31 @@
-import React from 'react';
-
+// Alert: Add List Name to list Invite Serializer * Backend *
+import React, { useState } from 'react'
+import ListInvitesControls from './ListInvitesControls'
+import ListInviteCard from './cards/ListInviteCard'
 import styled from 'styled-components'
 
 const Block = styled.div`
     width: 100%;
-    height: 20vh;
-    background-color: #CACACA;
-`; 
+    overflow-y: scroll;
+    ${'' /* height: 20vh; */}
+    ${'' /* background-color: #CACACA; */}
+`
 
 const ListInvitesContainer = ({ invites }) => {
-    const cards = invites.map(r => <p>{r.type}</p>)
+  const [filter, setFilter] = useState('received')
+  const cards = invites.filter(r => r.type === filter).map(r => <ListInviteCard record={r} />)
+
+  const filterChange = (event) => {
+    setFilter(event.target.id)
+  }
 
     return (
-        <Block>
-            <h3>Invites Component</h3>
-            {cards}
+      <Block>
+          <h3>Invites Component</h3>
+          <ListInvitesControls filter={filter} cb={filterChange} />
+          {cards}
         </Block>
-    );
+  )
 }
 
-export default ListInvitesContainer;
+export default ListInvitesContainer
