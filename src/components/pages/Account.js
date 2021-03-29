bce2da47e252d61d76f0c85f069aa1256c8fa48b
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 11:33:11 -0400

    created Account stub component

---
+++ src/components/pages/Account.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <div>
+      <h2>Account</h2>
+      <h2>edit info</h2>
+      <h2>log out</h2>
+      <h2>new password</h2>
+      <h2>delete account</h2>
+      <h2>stats?</h2>
+    </div>
+  )
+}
+
+export default Account
