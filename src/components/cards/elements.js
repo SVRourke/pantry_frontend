3d42df96d0013d4950c3f9c1971bf80d0d9de016
SVRourke <samuelvanrourke@gmail.com>
2021-03-29 15:07:48 -0400

    created card elements

---
+++ src/components/cards/elements.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const CardBody = styled.div`  
+  padding: 1.5rem 2rem 1rem 0;
+  width: 100%;
+`
+const CardHeading = styled.h3`
+  font-size: 2.7rem;
+  font-weight: 600;
+  color: inherit;
+`
+const MemberCount = styled.p`
+  font-weight: 800;
+`
+const TimeDisplay = styled.p`
+  font-weight: 500;
+  color: inherit;
+`
+
+export {
+    CardBody,
+    CardHeading,
+    MemberCount,
+    TimeDisplay
+}
