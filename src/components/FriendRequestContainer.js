670eac73b2cf2eb1b94d70dd57f6bcc8e9da370e
SVRourke <samuelvanrourke@gmail.com>
2021-03-30 08:58:14 -0400

    created friendrequestcontainer

--- src/components/FriendRequestContainer.js
+++ src/components/FriendRequestContainer.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
+import InvitesControls from './InvitesControls'
+import FriendRequestCard from './cards/FriendRequestCard'
+import styled from 'styled-components'
 
-const FriendRequestContainer = () => {
+const Block = styled.div`
+    width: 100%;
+    overflow-y: scroll;
+    ${'' /* height: 20vh; */}
+    ${'' /* background-color: #CACACA; */}
+`
+
+const FriendRequestContainer = ({ requests }) => {
+  const [filter, setFilter] = useState('received')
+  const cards = requests.filter(r => r.type === filter).map(r => <FriendRequestCard record={r} />)
+
+  const filterChange = (event) => { setFilter(event.target.id) }
+
   return (
-    <div>
-      
-    </div>
+    <Block>
+      <InvitesControls filter={filter} cb={filterChange} />
+      {cards}
+    </Block>
   );
 }
 
 export default FriendRequestContainer;
+
+
+
+
+
