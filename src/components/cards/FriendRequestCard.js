bc966043f5ff28cdca150e13c670c87d5ae1a4aa
SVRourke <samuelvanrourke@gmail.com>
2021-03-30 09:09:22 -0400

    created friendrequest card

---
+++ src/components/cards/FriendRequestCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  CardBody,
+  CardHeading,
+  TimeDisplay,
+  InvitedDisplay,
+  RequestCancel,
+  AcceptButton,
+  DeclineButton,
+  Row,
+  MemberCount
+} from './elements'
+
+
+
+
+const FriendRequestCard = ({ record }) => {
+  const { id, requestor_id, requestor_name, requestee_name, type, record_age } = record
+  const style = type === 'sent' ? { color: '#cccccc' } : { color: 'var(--offblack-color)' }
+  const heading = type === 'sent' ? requestee_name : requestor_name
+
+  return (
+    <CardBody typeStyle={type}>
+      <CardHeading style={style}>{heading}</CardHeading>
+
+      <Row>
+        <TimeDisplay>{record_age} ago</TimeDisplay>
+
+        <RequestCancel displayType={type}>
+          cancel?
+          </RequestCancel>
+      </Row>
+
+      <Row>
+        <AcceptButton displayType={type} >accept</AcceptButton>
+        <DeclineButton displayType={type} >decline</DeclineButton>
+      </Row>
+
+    </CardBody>
+  );
+}
+
+export default FriendRequestCard;
