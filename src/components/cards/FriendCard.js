8b2b473e772ec3981d5ca1adaa6cb6357ffde335
SVRourke <samuelvanrourke@gmail.com>
2021-03-30 09:09:10 -0400

    created friendCard

---
+++ src/components/cards/FriendCard.js
@@ -0,0 +1,28 @@
+// ALERT: Add mutual list count to friend serializer 
+import React from 'react';
+import {
+  CardBody,
+  CardHeading,
+  UnFriend,
+  TimeDisplay,
+  SubHeading,
+  Row
+} from "./elements";
+
+const FriendCard = (props) => {
+  const { friend_id, friend_name, id, record_age } = props.record
+  
+  return (
+    <CardBody>
+      <CardHeading>{friend_name}</CardHeading>
+      <Row>
+        <TimeDisplay>{record_age}</TimeDisplay>
+        {/* use id to post delete */}
+        <UnFriend>unfriend?</UnFriend> 
+      </Row>
+      <SubHeading>{'mutual listcount'}</SubHeading>
+    </CardBody>
+  );
+}
+
+export default FriendCard;
