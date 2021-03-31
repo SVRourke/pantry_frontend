c8d884ba420d0d93f66b1981d1da45d8340268af
SVRourke <samuelvanrourke@gmail.com>
2021-03-31 11:39:33 -0400

    removed console.log

--- src/friends/FriendList.js
+++ src/friends/FriendList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import FriendCard from './FriendCard'
 
 const FriendList = ({ records }) => {
-  console.log(records)
   const cards = records.map(r => <FriendCard record={r} />)
+  
   return (
     <div>
       {cards}
