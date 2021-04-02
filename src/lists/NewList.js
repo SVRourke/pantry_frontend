de34205a87dbbb6ba0cbb8044c55673987d755e5
SVRourke <samuelvanrourke@gmail.com>
2021-04-02 11:42:42 -0400

    stubbed out new list page

--- src/lists/NewList.js
+++ src/lists/NewList.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
 const NewList = () => {
-  let history = useHistory();
+  const history = useHistory();
+  const [ inputValue, setInput ] = useState("")
+
+  const inputHandler = (event) => { 
+    setInput(event.target.value)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    alert(`Creating ${inputValue}`)
+    setInput("")
+    // ALERT: MAKE DYNAMIC REDIRECT WITH RETURNED LIST ID VALUE
+  }
 
   return (
     <div>
       <h2>NEW LIST</h2>
-      <button
-        onClick={() => history.goBack()}
-      >cancel</button>
+      <button onClick={() => history.goBack()}>cancel</button>
+      <form onSubmit={submitHandler}>
+      <label for="list name">List Name</label>
+        <input onChange={inputHandler} type="text" name="list name" value={inputValue}/>
+        <input type="submit" value="create list" />
+      </form>
     </div>
   );
 }
