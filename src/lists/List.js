1ded6d61c0c677511a8bb6809d90b157e1a46048
SVRourke <samuelvanrourke@gmail.com>
2021-04-03 09:06:18 -0400

    updated to a strategy that would work better with redux

--- src/lists/List.js
+++ src/lists/List.js
@@ -5,6 +5,7 @@ import ItemsContainer from '../items/containers/ItemsContainer'
 import ItemCard from '../items/components/ItemCard'
 import EditItem from '../items/pages/EditItem'
 import NewItem from '../items/pages/NewItem'
+import MembersContainer from '../list_members/MembersContainer'
 import {
   Redirect,
   Route,
@@ -15,7 +16,7 @@ import {
 
 
 // REPLACED BY REDUX 
-import { TestLists, TestItems } from '../common/TestData'
+import { TestLists } from '../common/TestData'
 
 const List = () => {
   const { path, url } = useRouteMatch()
@@ -23,23 +24,23 @@ const List = () => {
   // REPLACED BY REDUX
   const listId = useParams().id
   const list = TestLists.find(l => l.id === parseInt(listId))
-  
-  // CREATE An Item Card
-  const items = TestItems.map(i => <ItemCard record={i} />)
+
 
   return (
     <div>
       <h1>{list.name}</h1>
       <Link to={`${url}/items`}>items</Link>
       <Link to={`${url}/members`}>users</Link>
-      <Link to={`/`}>home</Link>
+      {/* ALERT: Make Dynamic Call to Store? */}
+      <Link to={'/users/3'}>home</Link>
+
 
       <Route exact path={path} >
-        <Redirect to={`${url}/items`}/>
+        <Redirect to={`${url}/items`} />
       </Route >
 
       <Route path={`${path}/items/:id/edit`} >
-        <EditItem  />
+        <EditItem />
       </Route>
 
       <Route path={`${path}/items/new`} >
@@ -47,11 +48,11 @@ const List = () => {
       </Route>
 
       <Route exact path={`${path}/items`} >
-        <ItemsContainer records={TestItems} />
+        <ItemsContainer records={list.items} />
       </Route>
-      
+
       <Route path={`${path}/members`} >
-        <h1>users</h1>
+        <MembersContainer contributors={list.contributors} />
       </Route>
     </div>
   );
