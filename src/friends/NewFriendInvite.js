9ca905e0b6134ce6764b7089f6ce1e3904e0dce4
SVRourke <samuelvanrourke@gmail.com>
2021-04-06 08:34:55 -0400

    styled new friend page, added form

--- src/friends/NewFriendInvite.js
+++ src/friends/NewFriendInvite.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import InviteForm from '../common/InviteForm'
 import { useHistory } from "react-router-dom";
+import { Row } from '../common/elements'
+import styled from "styled-components"
+
+const PageHeading = styled.h1`
+  
+`;
+
+const GoBack = styled.button`
+  background: none;
+  border: none;
+  font-size: 1.5rem;
+  color: var(--gray-color);
+  padding-left: 1rem;
+
+  &:hover {
+    color: var(--red-color)
+  }
+`;
 
 const NewFriendInvite = () => {
   let history = useHistory();
-  console.log("TESTTESTESTEST")
+  // replace with dispatch
+  const handleForm = (info) => {
+    alert(info)
+  }
+
 
   return (
-    <div>
-      <h1>NEW FRIEND PAGE</h1>
-      <button
-        onClick={() => history.goBack()}
-      >cancel</button>
+    <div style={{
+      padding: "1rem 0"
+    }}>
+      <Row>
+        <PageHeading>add friend</PageHeading>
+        <GoBack
+          onClick={() => history.goBack()}
+        >cancel?</GoBack>
+      </Row>
+      <InviteForm cb={handleForm} />
+
     </div>
   );
 }
