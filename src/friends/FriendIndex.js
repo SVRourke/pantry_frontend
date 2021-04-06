fd9dae858bee1ae5d07cd41f608891baf18b074b
SVRourke <samuelvanrourke@gmail.com>
2021-04-06 08:33:42 -0400

    styled elements

--- src/friends/FriendIndex.js
+++ src/friends/FriendIndex.js
@@ -4,10 +4,24 @@ import FriendRequestContainer from '../friends/FriendRequestContainer'
 import NewFriendInvite from '../friends/NewFriendInvite'
 import { FancyLink } from '../common/GlobalElements'
 import { Route, Switch, Link, useRouteMatch } from 'react-router-dom'
-
+import styled from 'styled-components'
 // TEST DATA
 import { TestFriends, FriendRequests } from '../common/TestData';
 
+const NewFriend = styled(Link)`
+  border: solid var(--offblack-color) 2px;
+  padding: .3rem .7rem;
+  border-radius: 3rem;
+  width: 11ch;
+  margin: 1rem 0;
+
+  &:hover {
+    background: var(--offblack-color);
+    color: white;
+
+  }
+
+`;
 
 const FriendIndex = () => {
   const { path, url } = useRouteMatch()
@@ -15,11 +29,16 @@ const FriendIndex = () => {
   return (
     <Switch>
       <Route exact path={path} >
-        <FriendList records={TestFriends} />
+        <div style={{
+          height: "60vh",
+          display: "flex",
+          flexDirection: "column",
+          padding: "0 0 2rem 0"
+          }}>
+          <FriendList records={TestFriends} />
+          <NewFriend to={`${url}/new`}>add friend</NewFriend>
+        </div>
 
-        <FancyLink>
-          <Link to={`${url}/new`}>add friend</Link>
-        </FancyLink>
 
         <FriendRequestContainer requests={FriendRequests} />
       </Route>
