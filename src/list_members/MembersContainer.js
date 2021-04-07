9819c1367fa2fde3db0520870278f82bebc96470
SVRourke <samuelvanrourke@gmail.com>
2021-04-07 17:34:18 -0400

    styled, stubbed out routes, need to fix invite route

--- src/list_members/MembersContainer.js
+++ src/list_members/MembersContainer.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Link, Route, useRouteMatch } from 'react-router-dom'
 import { MemberCount } from '../common/elements';
 import MemberCard from './components/MemberCard'
+import styled from 'styled-components'
+
+const NiceLink = styled(Link)`
+  border: solid var(--offblack-color) 2px;
+  padding: .3rem .7rem;
+  border-radius: 3rem;
+  display: block;
+  width: fit-content;
+`;
+
 const MembersContainer = ({contributors}) => {
+  const { path, url } = useRouteMatch()
   const members = contributors.map(c => <MemberCard user={c} />)
 
   return (
     <div>
-      {members}
+
+      <Route exact path={path} >
+        {members}
+        {/* TODO: LINK TO Contributor INVITE PAGE */}
+        <NiceLink to={`${url}/invite`}>invite a user</NiceLink>
+      </Route>
+
+      <Route path={`${path}/invite`} >
+        <h1>INVITE MEMBER</h1>
+      </Route>
     </div>
   );
 }
 
 export default MembersContainer;
+// ALERT: FIX INVITE ROUTE
