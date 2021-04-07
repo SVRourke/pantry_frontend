0b5adcd7d7c5bd4573191bd29f8aca61984c70e7
SVRourke <samuelvanrourke@gmail.com>
2021-04-07 08:36:25 -0400

    refactored

--- src/items/containers/ItemsContainer.js
+++ src/items/containers/ItemsContainer.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import { Link, useRouteMatch } from "react-router-dom"
-import ItemCard from "../components/ItemCard"
+import { Link, useRouteMatch } from 'react-router-dom'
+import ItemCard from '../components/ItemCard'
 import styled from 'styled-components'
 
 const AddLink = styled(Link)`
     border: solid black 2px;
+    background-color: white;
     padding: .5rem 2rem;
     border-radius: 2rem;
     font-weight: 600;
-    ${'' /* position: absolute;
-    bottom: 5rem;
-    right: 5rem; */}
-`;
+    display: block;
+    width: fit-content;
+`
 
-export default function ItemContainer({records}) {
-    const { path, url } = useRouteMatch()
-    const cards = records.map(r => <ItemCard record={r} />)
-    return (
-        <div style={{height: '100vh', overflow: "hidden"}}>
-            <div style={{height: "80vh", overflowY: 'scroll'}}>
-                {cards}
-            </div>
-            <AddLink to={`${url}/new`} >new</AddLink>
-        </div>
-    )
+const containerStyles = {
+  height: '100vh',
+  overflow: 'hidden'
+}
+
+const innerContainerStyles = {
+  height: '80vh',
+  overflowY: 'scroll'
+}
+
+export default function ItemContainer ({ records }) {
+  const { path, url } = useRouteMatch()
+  const cards = records.map(r => <ItemCard record={r} />)
+
+  return (
+    <div style={containerStyles}>
+      <div style={innerContainerStyles}>
+        {cards}
+      </div>
+
+      <AddLink to={`${url}/new`}>new</AddLink>
+    </div>
+  )
 }
