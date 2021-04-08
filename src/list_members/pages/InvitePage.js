2ecfb892e64703aafbe30050b12b60f8e623558a
SVRourke <samuelvanrourke@gmail.com>
2021-04-08 15:16:34 -0400

    finished

--- src/list_members/pages/InvitePage.js
+++ src/list_members/pages/InvitePage.js
@@ -1,10 +1,20 @@
-// ALERT: DELETE
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 
+import InviteForm from '../../common/InviteForm'
+import { GoBack } from '../../common/FormElements'
+import { Row } from '../../common/elements'
 const InvitePage = () => {
+  const { goBack } = useHistory()
+
   return (
     <div>
-      
+      <Row>
+        <h2>invite a user</h2>
+        <GoBack onClick={() => goBack()}>cancel?</GoBack>
+      </Row>
+      {/* TODO: cb REDUX Action */}
+      <InviteForm cb={(data) => alert(data)} />
     </div>
   );
 }
