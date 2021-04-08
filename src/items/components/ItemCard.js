3a8f4388163ded37fd68a5e95b1dac429385a0a5
SVRourke <samuelvanrourke@gmail.com>
2021-04-08 16:31:16 -0400

    moved functions to container

--- src/items/components/ItemCard.js
+++ src/items/components/ItemCard.js
@@ -10,16 +10,8 @@ import {
 } from '../../common/elements'
 
 
-export default function ItemCard ({ record }) {
-  const toggleAcquired = () => {
-    alert(`marking ${record.name} acquired ID: ${record.id}`)
-  }
-  const trash = () => {
-    alert(`trashing ${record.name} ID: ${record.id}`)
-  }
-
+export default function ItemCard ({ record, toggle, trash }) {
   const { path, url } = useRouteMatch()
-
   return (
     <div style={{margin: '0 0 2rem 0'}}>
       <Route exact path={path}>
@@ -29,14 +21,14 @@ export default function ItemCard ({ record }) {
 
         <Row>
           <Acquired 
-            onClick={toggleAcquired}
+            onClick={() => toggle(record.id)}
             acquired={record.acquired}
           >
             acquired
           </Acquired>
 
           <Edit to={`${url}/${record.id}/edit`}>edit</Edit>
-          <Trash onClick={trash}>trash</Trash>
+          <Trash onClick={() => trash(record.id)}>trash</Trash>
         </Row>
       </Route>
 
