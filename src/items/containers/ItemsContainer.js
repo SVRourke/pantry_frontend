906d2a9fd5750d0c81fb07ce2657d63a14c1a734
SVRourke <samuelvanrourke@gmail.com>
2021-04-08 16:31:32 -0400

    added functions from item card

--- src/items/containers/ItemsContainer.js
+++ src/items/containers/ItemsContainer.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import ItemCard from '../components/ItemCard'
-import { AddLink, containerStyles, innerContainerStyles} from '../../common/elements'
+import { AddLink, containerStyles, innerContainerStyles } from '../../common/elements'
 
-// Attach To REDUX?
-export default function ItemContainer ({ records }) {
+// Attach To REDUX
+export default function ItemContainer({ records }) {
   const { url } = useRouteMatch()
-  const cards = records.map(r => <ItemCard record={r} />)
+
+  const toggleAcquired = (id) => {
+    const item = records.find(e => e.id === id)
+    alert(`TOGGLE: ${item.name}, ${item.id}`)
+  }
+
+  const trash = (id) => {
+    alert(`DELETE: ${id}`)
+  }
+  const cards = records.map(r => <ItemCard record={r} toggle={toggleAcquired} trash={trash} />)
 
   return (
     <div style={containerStyles}>
