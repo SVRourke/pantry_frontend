a76bff82b81b6cb3bfd176d1f3ceef1e439a195b
SVRourke <samuelvanrourke@gmail.com>
2021-04-09 15:44:18 -0400

    changed switched out placeholders for dynamic info

--- src/lists/components/ListInviteCard.js
+++ src/lists/components/ListInviteCard.js
@@ -13,7 +13,7 @@ import {
 
 
 const ListInviteCard = ({record, clickHandler}) => {
-  const { id, record_age, requestor_name, requestee_name, contributor_count, type } = record
+  const { id, list_name, record_age, requestor_name, requestee_name, contributor_count, type } = record
   
   const style = {
     color: type === 'sent' ? '#ccc' : 'var(--offblack-color)'
@@ -21,11 +21,11 @@ const ListInviteCard = ({record, clickHandler}) => {
 
   return (
     <CardBody typeStyle={type}>
-      <CardHeading style={style}>{'list_name'}</CardHeading>
+      <CardHeading style={style}>{type === 'sent' ? requestee_name : list_name}</CardHeading>
 
       <Row>
         <InvitedDisplay style={style}>
-          {type === 'sent' ? requestee_name : requestor_name}
+          {type === 'sent' ? list_name : requestor_name}
         </InvitedDisplay>
 
         <RequestCancel displayType={type} onClick={() => clickHandler(id, 'DELETE')} > 
