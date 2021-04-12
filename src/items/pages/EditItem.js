7b50b073a92191af8a27fbc62b3dc5d9334bdd20
SVRourke <samuelvanrourke@gmail.com>
2021-04-12 18:00:43 -0400

    connected to redux

--- src/items/pages/EditItem.js
+++ src/items/pages/EditItem.js
@@ -2,30 +2,37 @@ import React from 'react'
 import { useHistory, useParams, useRouteMatch } from 'react-router-dom'
 import ItemForm from '../components/ItemForm'
 import { CancelButton, Wrapper } from '../../common/elements'
-
-// REPLACE WITH CALL TO STORE
+import { connect } from 'react-redux'
+import { Edit } from '../../actions/ItemActions'
 import { TestItems } from '../../common/TestData'
 
-export default function EditItem () {
+const EditItem = ({ lists, editAction }) => {
   const history = useHistory()
   const { url } = useRouteMatch()
+  const { list_id, item_id } = useParams()
 
   const formHandler = (info) => {
-    alert(`Form Submitted ${info.name}: ${info.amount}|`)
+    editAction(list_id, { id: item_id, ...info })
     history.push(url.split('/').slice(0, url.split('/').length - 2).join('/'))
   }
 
-  // REPLACE WITH CALL TO STORE
-  const userId = parseInt(useParams().id)
-  const record = TestItems.find(i => i.id === userId)
+  const list = lists.find(l => l.id == list_id)
+  const record = list.items[parseInt(item_id)]
 
   return (
-      <div>
-          <Wrapper>
-              <h1>EDIT: {record.name}</h1>
-              <CancelButton onClick={() => history.goBack()}>cancel?</CancelButton>
-            </Wrapper>
-          <ItemForm cb={formHandler} record={record} />
-        </div>
+    <div>
+      <Wrapper>
+        <h1>EDIT: {record.name}</h1>
+        <CancelButton onClick={() => history.goBack()}>cancel?</CancelButton>
+        </Wrapper>
+      <ItemForm cb={formHandler} record={record} />
+    </div>
   )
 }
+const mapStateToProps = state => ({     
+  lists: state
+})
+const mapDispatchToProps = dispatch => ({
+  editAction: (listId, item) => dispatch(Edit(listId, item))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(EditItem)
