f9339cc2c0f2b5f23650573a0dc21cf7ef83ee0d
SVRourke <samuelvanrourke@gmail.com>
2021-04-12 13:41:52 -0400

    added Toggle acquired logic

--- src/reducers/ItemReducer.js
+++ src/reducers/ItemReducer.js
@@ -3,36 +3,38 @@ const TOGGLE = "TOGGLE"
 
 
 const itemReducer = (state, action) => {
+  const list = state.find(l => l.id === action.listId)
+
   
   switch (action.type) {
     // TOGGLE ACQUIRED
-    // case TOGGLE:
-    //   const list = state.find(l => l.id === action.listId)
-    //   const item = list.items.find(i => i.id === action.itemId)
-      
-    //   return [
-    //     ...state.filter(l => l.id !== action.listId),
-    //     {
-    //       ...list,
-    //       items: [
-    //         ...list.items.filter(i => i.id !== action.itemId),
-    //         {...item, acquired: !item.acquired}
-    //       ]
-    //     }
-    //   ]
-      // DELETE
-      case DELETE:
-        const list = state.find(l => l.id === action.listId)
+    case TOGGLE:
+      const item = list.items[action.itemId]
+      let { [action.itemId]: target, ...remainder } = list.items
+      console.log(target)
 
       return [
         ...state.filter(l => l.id !== action.listId),
         {
           ...list,
-          items: [
-            ...list.items.filter(i => i.id !== action.itemId)
-          ]
+          items: {
+            [action.itemId]: {...target, acquired: !target.acquired},
+            ...remainder,
+          }
         }
       ]
+
+    // DELETE
+    case DELETE:
+      let { [action.itemId]: value, ...newItems } = list.items
+
+      return [
+        ...state.filter(l => l.id !== action.listId),
+        { ...list, items: newItems}
+      ]
+
+    // EDIT
+    // NEW
 
     default:
       return state
