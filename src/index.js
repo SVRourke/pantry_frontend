2b6a02e9086a680db3b467d44d3ceff24a2127cc
SVRourke <samuelvanrourke@gmail.com>
2021-04-12 10:25:01 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
+import { Provider } from "react-redux"
+import configureStore from "./store.js"
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={configureStore()} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
