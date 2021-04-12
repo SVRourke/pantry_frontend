5c969ed80d991a0b9660f9361ab21a3712d05153
SVRourke <samuelvanrourke@gmail.com>
2021-04-12 10:26:02 -0400

    created store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+import itemReducer from "./reducers/ItemReducer"
+import { TestLists } from "./common/TestData"
+
+
+function configureStore(state = TestLists) {
+  return createStore(itemReducer, state)
+}
+export default configureStore
