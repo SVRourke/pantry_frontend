7ae3d8962ee0ebcd708f9e6762cc113aa065ac87
SVRourke <samuelvanrourke@gmail.com>
2021-04-14 12:00:04 -0400

    created login reducer

---
+++ src/reducers/Login.js
@@ -0,0 +1,22 @@
+const PENDING = 'LOGINPENDING'
+const SUCCESS = 'LOGGEDIN'
+const FAILURE = 'LOGINFAILURE'
+
+const initialState = {
+  isLoggedIn: 'Logged Out',
+  userId: ''
+}
+
+const LoginReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case PENDING:
+      return { ...state, isLoggedIn: 'pending' }
+    case SUCCESS:
+      return {...state, isLoggedIn: 'TRUE' }
+    case FAILURE:
+      return {...state, isLoggedIn: 'FAILED'}
+    default:
+      return state
+  }
+}
+export default LoginReducer
