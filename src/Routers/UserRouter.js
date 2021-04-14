9dc1d4ff95e5eceeb19e9d34d67c484739b60ed1
SVRourke <samuelvanrourke@gmail.com>
2021-04-14 17:29:32 -0400

    formatting

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -10,7 +10,8 @@ import ListsIndex from '../lists/ListsIndex'
 import Friends from '../friends/FriendIndex'
 import Account from '../Account'
 import MainNav from '../common/MainNav'
-const UserRouter = () => {
+
+const UserRouter = (props) => {
   const { path, url } = useRouteMatch()
 
   return (
