5668596f0e6a0d564949cd182ad19974945047f6
SVRourke <samuelvanrourke@gmail.com>
2021-04-14 09:51:58 -0400

    stubbed out logout function

--- src/Account.js
+++ src/Account.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Interface, Schemas } from './api/Interface'
 const Account = () => {
+  const logout = () => {
+    Interface('logout', Schemas['logout'])
+      .then(d => {
+        console.log(d)
+      })
+      .catch(error => {
+        console.log(error)
+        alert('Log Out Failed, try again soon...')
+      })
+  }
   return (
     <div>
+      <button onClick={logout} >Logout</button>
       <h2>Account</h2>
       <h2>edit info</h2>
       <h2>log out</h2>
