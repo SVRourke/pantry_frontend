63586a94055769a920330a9b82488e4d005d8ef2
SVRourke <samuelvanrourke@gmail.com>
2021-04-14 11:58:48 -0400

    added redux-thunk, created login reducer for handling user auth

--- src/store.js
+++ src/store.js
@@ -1,13 +1,16 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
 import itemReducer from "./reducers/Items"
 import listInviteReducer from './reducers/ListInvites'
-
+import LoginReducer from './reducers/Login'
 
 const rootReducer = combineReducers({
   lists: itemReducer,
   listInvites: listInviteReducer,
+  profile: LoginReducer,
 })
-const combinedStore = createStore(rootReducer)
+const combinedStore = createStore(rootReducer, applyMiddleware(thunk))
 export default combinedStore
 
 
