d557b8a8630f59e720582d6cbe74208d4c653f7a
SVRourke <samuelvanrourke@gmail.com>
2021-04-15 08:07:08 -0400

    using useEffect to trigger load lists thunk

--- src/lists/containers/ListCardContainer.js
+++ src/lists/containers/ListCardContainer.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ListCard from '../components/ListCard'
 import { Block } from '../../common/elements'
 
-const ListCardContainer = ({ records }) => {
+import { connect } from 'react-redux'
+import {LoadLists} from '../../actions/ItemActions'
+
+
+const ListCardContainer = ({ records, lists, userId, loggedIn, load }) => {
   const cards = records.map(r => <ListCard key={r.id} list={r} />)
+
+  useEffect(() => {
+    if (lists.length === 0) {
+      load(userId)
+    }
+  })
 
   return (
     <Block>
       {cards}
     </Block>
+    
   )
 }
 
-export default ListCardContainer
+const mapStateToProps = state => ({
+  lists: state.lists,
+  userId: state.profile.userId,
+  loggedIn: state.profile.isLoggedIn
+})
+const mapDispatchToProps = dispatch => {
+  return {
+    load: userId => dispatch(LoadLists(userId))
+  }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListCardContainer)
+
+// Connects to slice of redux store
