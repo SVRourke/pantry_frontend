80693eff28ef7321229f408da35f6e3476765dc8
SVRourke <samuelvanrourke@gmail.com>
2021-04-15 08:41:01 -0400

    mapdispatchtoprops, authcheck if not loggedIn, return ternary

--- src/common/PrivateRoute.js
+++ src/common/PrivateRoute.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
+import { authCheck } from '../actions/LoginActions'
 
-const PrivateRoute = (props) => {
-  const { loggedIn, userId, path, component, ...others } = props
+const PrivateRoute = ({ loggedIn, userId, path, component, authCheck, ...others }) => {
+  if (loggedIn !== 'TRUE') {
+    authCheck()
+  }
 
-  return loggedIn === 'TRUE' ? <Route path={path} component={component} /> : <Redirect to='/login' />
+  return (
+    loggedIn === 'TRUE'
+      ? <Route path={path} component={component} />
+      : <Redirect to='/login' />
+  )
 }
 
 const mapStateToProps = state => ({
@@ -13,8 +20,9 @@ const mapStateToProps = state => ({
   loggedIn: state.profile.isLoggedIn
 })
 
-// const mapDispatchToProps = dispatch => {
-// CHECK AUTH THUNK
-// }
-
-export default connect(mapStateToProps,)(PrivateRoute);
+const mapDispatchToProps = dispatch => {
+  return {
+    authCheck: () => dispatch(authCheck())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute);
