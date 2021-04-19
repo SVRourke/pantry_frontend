a9d199ab42184417ce1f7c563b9f3d602867b7ec
SVRourke <samuelvanrourke@gmail.com>
2021-04-19 09:20:11 -0400

    using thunk now

--- src/items/pages/EditItem.js
+++ src/items/pages/EditItem.js
@@ -3,19 +3,22 @@ import { connect } from 'react-redux'
 import { useHistory, useParams, useRouteMatch } from 'react-router-dom'
 import { CancelButton, Wrapper } from '../../common/elements'
 import ItemForm from '../components/ItemForm'
-import { Edit } from '../../actions/ItemActions'
+import { updateItem } from '../../actions/ItemActions'
 
-const EditItem = ({ lists, editAction }) => {
+const EditItem = (props) => {
+  const { items, editAction } = props
+
   const history = useHistory()
   const { url } = useRouteMatch()
+
   const { list_id, item_id } = useParams()
+
+  const record = items.find(i => i.id === parseInt(item_id))
 
   const formHandler = (info) => {
     editAction(list_id, { id: item_id, ...info })
     history.push(url.split('/').slice(0, url.split('/').length - 2).join('/'))
   }
-
-  const record = lists.find(l => l.id === parseInt(list_id)).items[parseInt(item_id)]
 
   return (
     <div>
@@ -31,6 +34,6 @@ const mapStateToProps = state => ({
   ...state
 })
 const mapDispatchToProps = dispatch => ({
-  editAction: (listId, item) => dispatch(Edit(listId, item))
+  editAction: (listId, item) => dispatch(updateItem(listId, item))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(EditItem)
