2aef9cd7e83f91e779c2a412e039b65ea49c77f0
SVRourke <samuelvanrourke@gmail.com>
2021-04-19 09:21:20 -0400

    fixed load action response issue causing duplication of items, fixed edit action response

--- src/reducers/Items.js
+++ src/reducers/Items.js
@@ -1,32 +1,35 @@
 const DELETE = 'DELETE'
 const TOGGLE = 'TOGGLE'
-const CREATE = 'CREATE'
+// const CREATE = 'CREATE'
 const EDIT = 'EDIT'
 const LOAD = 'LOADITEMS'
 
 const itemReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD:
-      return [...state, ...action.items]
+      return action.items
 
     // TOGGLE ACQUIRED
     case TOGGLE:
       const target = state.find(i => i.id === action.itemId)
-      
+
       return [
-        Object.assign({}, target, {acquired: !target.acquired}),
+        Object.assign({}, target, { acquired: !target.acquired }),
         ...state.filter(i => i.id !== action.itemId),
+      ]
+
+    // EDIT
+    case EDIT:
+      const old = state.find(i => i.id === action.item.id)
+      return [
+        Object.assign({}, old, action.item),
+        ...state.filter(i => i.id !== action.item.id)
       ]
 
     // DELETE
     case DELETE:
       const { [action.itemId]: value, ...untouched } = state
       return [...untouched]
-
-    // EDIT
-    case EDIT:
-      const { [action.item.id]: old, ...nonupdated } = state
-      return [...state]
 
     // DEFAULT
     default:
