54a17b8493f16ebe4a38c2db6cb8644313a899b7
SVRourke <samuelvanrourke@gmail.com>
2021-04-20 13:27:30 -0400

    added memberReducer

--- src/store.js
+++ src/store.js
@@ -4,12 +4,14 @@ import listReducer from './reducers/Lists'
 import itemReducer from "./reducers/Items"
 import listInviteReducer from './reducers/ListInvites'
 import LoginReducer from './reducers/Login'
+import MemberReducer from './reducers/ListMembers'
 
 const rootReducer = combineReducers({
   lists: listReducer,
   items: itemReducer,
+  members: MemberReducer,
   listInvites: listInviteReducer,
-  profile: LoginReducer,
+  profile: LoginReducer
 })
 const combinedStore = createStore(rootReducer, applyMiddleware(thunk))
 export default combinedStore
