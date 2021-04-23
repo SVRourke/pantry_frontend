96a5e78b4342b52ae771011ef5a08996359da6f6
SVRourke <samuelvanrourke@gmail.com>
2021-04-23 12:11:47 -0400

    formatting, added add to reducer

--- src/reducers/Lists.js
+++ src/reducers/Lists.js
@@ -1,10 +1,16 @@
 const LOAD = 'LOADLISTS'
-
+const ADD = 'ADDLIST'
 
 const listReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD:
-      return [...action.lists]
+      return action.lists
+
+    case ADD:
+      return [
+        ...state,
+        action.list
+      ]
 
     // DEFAULT
     default:
