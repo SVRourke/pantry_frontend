a2736916eafaee779575e6778e4b322cde818123
SVRourke <samuelvanrourke@gmail.com>
2021-04-23 17:41:54 -0400

    connected to redux, useeffect on load thunk and accept on click from card

--- src/friends/FriendRequestContainer.js
+++ src/friends/FriendRequestContainer.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import InvitesControls from '../common/InvitesControls'
 import FriendRequestCard from '../friends/FriendRequestCard'
 import { Block } from '../common/elements'
 
-const FriendRequestContainer = ({ requests }) => {
+import { connect } from "react-redux"
+import { 
+  loadRequests,
+  acceptRequest
+ } from "../actions/FriendRequestActions"
+
+const FriendRequestContainer = ({ userId, load, accept, requests }) => {
+  useEffect(() => {
+    load(userId)
+  }, [])
+
   const handleRequest = (id, action) => {
-    switch(action) {
+    switch (action) {
       case 'CANCEL':
         alert(`CANCEL ID: ${id}`)
         break;
       case 'ACCEPT':
         alert(`ACCEPT ID: ${id}`)
-        break;
-      case 'DECLINE':
-        alert(`DENY ID: ${id}`)
+        accept(userId, id)
         break;
       default:
         return false
@@ -21,16 +29,31 @@ const FriendRequestContainer = ({ requests }) => {
   }
 
   const [filter, setFilter] = useState('received')
-  const cards = requests.filter(r => r.type === filter).map(r => <FriendRequestCard record={r} clickHandler={handleRequest}key={r.id} />)
+  const cards = requests.filter(r => r.type === filter).map(r => {
+    return (
+      <FriendRequestCard
+        record={r}
+        clickHandler={handleRequest}
+        key={r.id}
+      />
+    )
+  })
 
   const filterChange = (event) => { setFilter(event.target.id) }
 
   return (
     <Block>
       <InvitesControls filter={filter} cb={filterChange} />
-      {cards}
+      {cards.length ? cards : <p>No requests...</p>}
     </Block>
   );
 }
-
-export default FriendRequestContainer;
+const mapStateToProps = state => ({
+  userId: state.profile.userId,
+  requests: state.friendRequests
+})
+const mapDispatchToProps = dispatch => ({
+  load: (userId) => dispatch(loadRequests(userId)),
+  accept: (userId, reqId) => dispatch(acceptRequest(userId, reqId))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(FriendRequestContainer)
