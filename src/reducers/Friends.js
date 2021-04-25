4b63f39e117754cc7c3591bf277c0b031ff82a39
SVRourke <samuelvanrourke@gmail.com>
2021-04-25 11:43:13 -0400

    created friends reducer

---
+++ src/reducers/Friends.js
@@ -0,0 +1,16 @@
+const LOAD = 'LOADFRIENDS'
+const UNFRIEND = 'UNFRIEND'
+
+
+const FriendsReducer = (state = [], action) => {
+  switch (action.type) {
+    case LOAD:
+      return action.friends
+    case UNFRIEND:
+      return state.filter((r) => r.id !== action.id)
+    default:
+      return state
+  }
+}
+
+export default FriendsReducer
