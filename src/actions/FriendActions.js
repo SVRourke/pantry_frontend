d8da1c6eec2df1c8146cd9a4284096552f76f39a
SVRourke <samuelvanrourke@gmail.com>
2021-04-25 11:35:51 -0400

    added load action creator to add friends to store, created loadfriends thunk to fetch friends from the api

--- src/actions/FriendActions.js
+++ src/actions/FriendActions.js
@@ -1,3 +1,5 @@
+import Api from '../api/Interface'
+
 // UNFRIEND
 const Unfriend = (friendshipId) => {
   return {
@@ -5,4 +7,33 @@ const Unfriend = (friendshipId) => {
     friendshipId: friendshipId
   }
 }
-export { Unfriend }
+
+const Load = friends => {
+  return {
+    type: 'LOADFRIENDS',
+    friends
+  }
+}
+
+const loadFriends = userId => {
+  return async dispatch => {
+    Api.loadFriends(userId)
+      .then(r => {
+        return (
+          r.ok
+            ? r.json()
+            : Promise.reject(r)
+        )
+      })
+      .then(d => {
+        dispatch(Load(d))
+      })
+      .catch(error => console.log("LOAD FRIENDS ERROR", error))
+  }
+}
+
+
+export {
+  Unfriend,
+  loadFriends
+}
