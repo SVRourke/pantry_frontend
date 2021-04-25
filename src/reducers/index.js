d1a4e477227e263b83d5ba9e104e828b950d4926
SVRourke <samuelvanrourke@gmail.com>
2021-04-25 11:43:33 -0400

    created index to make importing reducers easier

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+import listReducer from './Lists'
+import itemReducer from "./Items"
+import listInviteReducer from './ListInvites'
+import LoginReducer from './Login'
+import MemberReducer from './ListMembers'
+import FriendRequestReducer from './friendRequests'
+import FriendsReducer from "./Friends"
+
+export {
+  listReducer,
+  itemReducer,
+  listInviteReducer,
+  LoginReducer,
+  MemberReducer,
+  FriendRequestReducer,
+  FriendsReducer
+}
