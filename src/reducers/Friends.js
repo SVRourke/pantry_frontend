68e024526d19f738e7a97a38f65467679377871a
SVRourke <samuelvanrourke@gmail.com>
2021-04-26 09:08:31 -0400

    created action to add a new friend when a request is accepted

--- src/reducers/Friends.js
+++ src/reducers/Friends.js
@@ -1,11 +1,16 @@
 const LOAD = 'LOADFRIENDS'
 const UNFRIEND = 'UNFRIEND'
-
+const ADD = 'ADDFRIEND'
 
 const FriendsReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD:
       return action.friends
+    case ADD:
+      return [
+        ...state,
+        action.friend
+      ]
     case UNFRIEND:
       return state.filter((r) => r.id !== action.friendshipId)
     default:
