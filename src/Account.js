8ec1b85c66998115890726cd87bf842c979008a0
SVRourke <samuelvanrourke@gmail.com>
2021-04-29 17:06:49 -0400

    added profile req, comments

--- src/Account.js
+++ src/Account.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { logout } from './actions/LoginActions'
-import api from './api/Index'
+import { logout, profile } from './actions/LoginActions'
+// import api from './api/Index'
 
 const Account = (props) => {
-  const { logout, history } = props
+  const { logout, history, userId, profile } = props
+
+  useEffect(() => {
+    profile(userId)
+  }, [])
 
   const handleLogout = () => {
     logout(() => history.push("/"))
@@ -14,10 +18,15 @@ const Account = (props) => {
     <div>
       <button onClick={handleLogout} >Logout</button>
 
+      {/* LIST COUNT */}
+      {/* ITEM COUNT */}
+      {/* FRIEND COUNT */}
+      {/* LOGOUT */}
+      {/* DELETE ACCOUNT */}
+
+
       <h2>Account</h2>
-      <h2>edit info</h2>
       <h2>log out</h2>
-      <h2>new password</h2>
       <h2>delete account</h2>
       <h2>stats?</h2>
     </div>
@@ -26,6 +35,9 @@ const Account = (props) => {
 
 const mapDispatchToProps = dispatch => ({
   logout: (cb) => dispatch(logout(cb)),
+  profile: (userId) => dispatch(profile(userId))
 })
-
-export default connect(null, mapDispatchToProps)(Account)
+const mapStateToProps = state => ({
+  userId: state.profile.userId
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Account)
