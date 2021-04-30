49c6b2e7364ffa61271e78b3132f5bbb630b9266
SVRourke <samuelvanrourke@gmail.com>
2021-04-30 08:03:29 -0400

    added stats display

--- src/Account.js
+++ src/Account.js
@@ -1,43 +1,39 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { logout, profile } from './actions/LoginActions'
-// import api from './api/Index'
 
-const Account = (props) => {
-  const { logout, history, userId, profile } = props
-
+const Account = ({ logout, history, getProfile, profile }) => {
   useEffect(() => {
-    profile(userId)
+    getProfile(profile.userId)
   }, [])
 
   const handleLogout = () => {
-    logout(() => history.push("/"))
+    logout(() => history.push('/login'))
   }
 
   return (
     <div>
-      <button onClick={handleLogout} >Logout</button>
 
-      {/* LIST COUNT */}
-      {/* ITEM COUNT */}
-      {/* FRIEND COUNT */}
-      {/* LOGOUT */}
-      {/* DELETE ACCOUNT */}
+      <h1>hi {profile.name}!</h1>
+      <p>email: {profile.email}</p>
 
-
-      <h2>Account</h2>
-      <h2>log out</h2>
+      <h3>lists: {profile.list_count}</h3>
+      <h3>friends: {profile.friend_count}</h3>
+      <h3>Friend Requests</h3>
+      <p>sent: {profile.sent_requests} received: {profile.received_requests}</p>
+      <h3>list invites</h3>
+      <p>sent: {profile.sent_invites} received: {profile.received_invites}</p>
       <h2>delete account</h2>
-      <h2>stats?</h2>
+      <button onClick={handleLogout} >Logout</button>
     </div>
   )
 }
 
 const mapDispatchToProps = dispatch => ({
   logout: (cb) => dispatch(logout(cb)),
-  profile: (userId) => dispatch(profile(userId))
+  getProfile: (userId) => dispatch(profile(userId))
 })
 const mapStateToProps = state => ({
-  userId: state.profile.userId
+  profile: state.profile
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Account)
