d9196b26f91788e7f75ad66c3a9ee3064892e044
SVRourke <samuelvanrourke@gmail.com>
2021-04-30 11:01:47 -0400

    added leavelist logic

--- src/reducers/ListMembers.js
+++ src/reducers/ListMembers.js
@@ -1,10 +1,12 @@
 const LOAD = 'LOADMEMBERS'
-
+const LEAVELIST ='LEAVE'
 const MemberReducer = (state=  [], action) => {
+  console.log("REDUCER", action)
   switch(action.type) {
     case LOAD:
       return action.members
-
+    case LEAVELIST:
+      return []
     default:
       return state
   }
