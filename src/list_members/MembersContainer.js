2d0840be7e2ec2745e4f50ec3b146d8a5aa2924b
SVRourke <samuelvanrourke@gmail.com>
2021-04-30 11:04:17 -0400

    integrated leave list functionality

--- src/list_members/MembersContainer.js
+++ src/list_members/MembersContainer.js
@@ -1,33 +1,44 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { useRouteMatch, useParams } from 'react-router-dom'
+import { useRouteMatch, useParams, useHistory } from 'react-router-dom'
 import MemberCard from './components/MemberCard'
 import { NiceLink } from '../common/elements'
 
 import {
   loadMembers,
+  leaveList
 } from '../actions/ListMemberActions'
 
-const MembersContainer = ({ members, load }) => {
+const MembersContainer = (props) => {
+  const { members, userId, load, leave } = props
   const {  url } = useRouteMatch()
   const {list_id} = useParams()
+  const history = useHistory()
+  
 
   useEffect(() => {
     load(list_id)
   }, [])
 
+  const handleLeave = () => {
+    leave(list_id, (() => history.push(`/users/${userId}/lists`)))
+  }
+
   return (
     <div>
       { members.map(c => <MemberCard user={c} />) }
       <NiceLink to={`${url}/invite`}>invite a user</NiceLink>
+      <button onClick={handleLeave} >leave</button>
     </div>
   );
 }
 
 const mapStateToProps = state => ({
-  members: state.members
+  members: state.members,
+  userId: state.profile.userId
 })
 const mapDispatchToProps = dispatch => ({
-  load: (list_id) => dispatch(loadMembers(list_id))
+  load: (list_id) => dispatch(loadMembers(list_id)),
+  leave: (list_id, cb) => dispatch(leaveList(list_id, cb))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(MembersContainer)
