0cf26c094004a6115bc8cda48cc607dc8a775294
SVRourke <samuelvanrourke@gmail.com>
2021-04-30 08:07:11 -0400

    updated for profile info

--- src/reducers/Login.js
+++ src/reducers/Login.js
@@ -1,17 +1,29 @@
-const SUCCESS = 'LOGGEDIN'
-const FAILURE = 'LOGINFAILURE'
-const LOGOUT = "LOGOUT"
+const LOGIN = 'LOGGEDIN'
+const LOGOUT = 'LOGOUT'
+const PROFILE = 'GETPROFILE'
+const RESET = 'RESET'
+
 const initialState = {
-  isLoggedIn: 'Logged Out',
-  userId: ''
+  isLoggedIn: false,
+  userId: '',
+  email: '',
+  name: '',
+  list_count: 0,
+  friend_count: 0,
+  sent_requests: 0,
+  received_requests: 0
 }
 
-const LoginReducer = (state=initialState, action) => {
-  switch(action.type) {
-    case SUCCESS:
-      return {...state, isLoggedIn: 'TRUE', userId: action.id }
+const LoginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return { ...state, isLoggedIn: true, userId: action.id }
     case LOGOUT:
-      return { isLoggedIn: '', userId: '' }
+      return initialState
+    case PROFILE:
+      return { ...state, ...action.info}
+    case RESET:
+      return initialState
     default:
       return state
   }
