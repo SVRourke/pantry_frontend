5a0088a746b2609bcb90bb63aa85811b02c2b4ad
SVRourke <samuelvanrourke@gmail.com>
2021-05-03 08:17:37 -0400

    added signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import Welcome from './pages/Welcome'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import UserRouter from './Routers/UserRouter'
 import List from './lists/List'
 function App () {
@@ -11,6 +12,7 @@ function App () {
       <Switch>
         <Route exact path='/' component={Welcome} />
         <Route path='/login' component={Login} />
+        <Route path='/sign-up' component={SignUp} />
         <Route path={`/users/:user_id`} component={UserRouter} />
         <Route path={`/lists/:list_id`} component={List} />
       </Switch>
