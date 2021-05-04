80331d6489d761295420a4441374001013d12d2e
SVRourke <samuelvanrourke@gmail.com>
2021-05-04 08:02:13 -0400

    remove unused route, why was it there?

--- src/items/components/ItemCard.js
+++ src/items/components/ItemCard.js
@@ -10,28 +10,25 @@ import {
 } from '../../common/elements'
 
 
-export default function ItemCard ({ record, toggle, trash }) {
+export default function ItemCard({ record, toggle, trash }) {
   const { path, url } = useRouteMatch()
   return (
-    <div style={{margin: '0 0 2rem 0'}}>
-      <Route exact path={path}>
-        <CardHeading>{record.name}</CardHeading>
+    <div style={{ margin: '0 0 2rem 0' }}>
+      <CardHeading>{record.name}</CardHeading>
 
-        <MemberCount>{record.amount}</MemberCount>
+      <MemberCount>{record.amount}</MemberCount>
 
-        <Row>
-          <Acquired 
-            onClick={() => toggle(record.id)}
-            acquired={record.acquired}
-          >
-            acquired
+      <Row>
+        <Acquired
+          onClick={() => toggle(record.id)}
+          acquired={record.acquired}
+        >
+          acquired
           </Acquired>
 
-          <Edit to={`${url}/${record.id}/edit`}>edit</Edit>
-          <Trash onClick={() => trash(record.id)}>trash</Trash>
-        </Row>
-      </Route>
-
+        <Edit to={`${url}/${record.id}/edit`}>edit</Edit>
+        <Trash onClick={() => trash(record.id)}>trash</Trash>
+      </Row>
     </div>
   )
 }
