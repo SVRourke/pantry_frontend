dc84474a0d59782939979f7517d39dd291911857
SVRourke <samuelvanrourke@gmail.com>
2021-05-06 14:33:00 -0400

    removed env test elements

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
-  const { REACT_APP_API_DOMAIN } = process.env
   return (
     <div>
       <h1>Welcome</h1>
-      <p>first {REACT_APP_API_DOMAIN}</p>
-      <p>second {process.env.REACT_APP_API_DOMAIN}</p>
       <Link to='/login'>login</Link><br/>
       <Link to='/sign-up'>sign up</Link>
     </div>
