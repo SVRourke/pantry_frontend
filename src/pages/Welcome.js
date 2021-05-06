e8b16756111a9394f9e1a33204fde6f6d2b6149f
SVRourke <samuelvanrourke@gmail.com>
2021-05-06 17:38:00 -0400

    removed test

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
-  const { REACT_APP_API_DOMAIN } = process.env
   return (
     <div>
-    <h2>{REACT_APP_API_DOMAIN}</h2>
       <h1>Welcome</h1>
       <Link to='/login'>login</Link><br/>
       <Link to='/sign-up'>sign up</Link>
