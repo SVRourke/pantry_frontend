0f1e22a2baa361ffa91d23db112765a2ee3e6c5b
SVRourke <samuelvanrourke@gmail.com>
2021-05-13 07:57:55 -0400

    created and added stylesheet for welcom page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './welcome.scss'
 const Welcome = () => {
   return (
-    <div>
+    <div className={'welcome'}>
       <h1>Welcome</h1>
       <Link to='/login'>login</Link><br/>
       <Link to='/sign-up'>sign up</Link>
+
+
     </div>
   )
 }
