6b284c8fbe9ecaefb7e73132ccb83372921c16b2
SVRourke <samuelvanrourke@gmail.com>
2021-05-21 08:08:59 -0400

    rollback

--- src/api/Interface.js
+++ src/api/Interface.js
@@ -34,7 +34,7 @@ const buildOptions = (method, authed, body, token) => {
 }
 
 export const baseRequest = (endpoint, method, authed, body) => {
-  console.log('testo cookie', document.cookie.replace(/(?:(?:^|.*;\s*)CSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, "$1");  )
+  console.log('testo cookie', Cookies.get(), document.cookie)
   return (
     fetch(
       `${BASEURL}${endpoint}`,
