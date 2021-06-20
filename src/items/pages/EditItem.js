975a0d5d892a646380c8a6b5199c308400851309
SVRourke <samuelvanrourke@gmail.com>
2021-06-20 09:46:31 -0400

    format, update state mapping, add load and clear, use load and clear

--- src/items/pages/EditItem.js
+++ src/items/pages/EditItem.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { useHistory, useParams, useRouteMatch } from "react-router-dom";
 import ItemForm from "../components/ItemForm";
-import { updateItem } from "../../actions/ItemActions";
+import { updateItem, LoadItems, clearItems } from "../../actions/ItemActions";
 
-const EditItem = (props) => {
-  const { items, editAction } = props;
-
+const EditItem = ({ items, editAction, load, clear }) => {
   const history = useHistory();
   const { url } = useRouteMatch();
-
   const { list_id, item_id } = useParams();
 
-  const record = items.find((i) => i.id === parseInt(item_id));
+  const [record, set] = useState({
+    ...items.find((i) => i.id === parseInt(item_id)),
+  });
+
+  useEffect(() => {
+    load(list_id);
+
+    return () => {
+      clear();
+    };
+  }, []);
 
   const formHandler = (info) => {
     editAction(list_id, { id: item_id, ...info });
@@ -37,9 +44,11 @@ const EditItem = (props) => {
   );
 };
 const mapStateToProps = (state) => ({
-  ...state,
+  items: state.items,
 });
 const mapDispatchToProps = (dispatch) => ({
   editAction: (listId, item) => dispatch(updateItem(listId, item)),
+  load: (listId) => dispatch(LoadItems(listId)),
+  clear: () => dispatch(clearItems()),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(EditItem);
