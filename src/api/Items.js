6090a6ad9aaa80b161b76b51219a249b0098c2c4
SVRourke <samuelvanrourke@gmail.com>
2021-06-20 09:44:10 -0400

    formatting, added read action

--- src/api/Items.js
+++ src/api/Items.js
@@ -1,53 +1,38 @@
-import { baseRequest } from './Interface'
+import { baseRequest } from "./Interface";
 
 const create = (listId, item) => {
-  return baseRequest(
-    `lists/${listId}/items`,
-    'POST',
-    true,
-    {item: item}
-  )
-}
+  return baseRequest(`lists/${listId}/items`, "POST", true, { item: item });
+};
 
 const destroy = (listId, itemId) => {
-  return baseRequest(
-    `lists/${listId}/items/${itemId}`,
-    'DELETE',
-    true,
-  )
-}
+  return baseRequest(`lists/${listId}/items/${itemId}`, "DELETE", true);
+};
 
 const update = (listId, item) => {
-  return baseRequest(
-    `lists/${listId}/items/${item.id}/update`,
-    'PUT',
-    true,
-    {item: item}
-  )
-}
+  return baseRequest(`lists/${listId}/items/${item.id}/update`, "PUT", true, {
+    item: item,
+  });
+};
 
 const toggle = (listId, itemId) => {
-  return baseRequest(
-    `lists/${listId}/items/${itemId}/acquire`,
-    'PATCH',
-    true,
-  )
-}
+  return baseRequest(`lists/${listId}/items/${itemId}/acquire`, "PATCH", true);
+};
 
 const load = (listId) => {
-  return baseRequest(
-    `lists/${listId}/items`,
-    'GET',
-    true,
-  )
-}
+  return baseRequest(`lists/${listId}/items`, "GET", true);
+};
+
+const read = (listId, itemId) => {
+  return baseRequest(`lists/${listId}/items/${itemId}`, "GET", true);
+};
 
 const items = {
   create,
   destroy,
   update,
   toggle,
-  load
-}
+  read,
+  load,
+};
 
-export default items
+export default items;
