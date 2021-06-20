e2553117497eaf53eb96ace39e90a76501d12d88
SVRourke <samuelvanrourke@gmail.com>
2021-06-20 10:03:38 -0400

    added text to troubleshoot deploy

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,63 +1,63 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { handleLogin } from '../actions/LoginActions'
-import { Redirect } from 'react-router-dom'
-import './login.scss'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { handleLogin } from "../actions/LoginActions";
+import { Redirect } from "react-router-dom";
+import "./login.scss";
 
 const Login = ({ userId, isLoggedIn, login }) => {
   const [input, setInput] = useState({
     email: "",
     password: "",
-  })
+  });
 
-  const submitHandler = event => {
-    event.preventDefault()
-    login(input)
-  }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    login(input);
+  };
 
-  const changeHandler = event => {
+  const changeHandler = (event) => {
     setInput({
       ...input,
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   return (
-    <div className={'login'}>
+    <div className={"login"}>
       {isLoggedIn ? <Redirect to={`/users/${userId}`} /> : false}
 
       <h1>Login</h1>
+      <p>test text</p>
 
-      <form onSubmit={event => submitHandler(event)} >
+      <form onSubmit={(event) => submitHandler(event)}>
         <input
           name="email"
           type="email"
-          value={input['email']}
-          placeholder='email'
-          onChange={event => changeHandler(event)} />
+          value={input["email"]}
+          placeholder="email"
+          onChange={(event) => changeHandler(event)}
+        />
 
         <input
           name="password"
           type="password"
-          placeholder='password'
-          value={input['password']}
-          onChange={event => changeHandler(event)} />
+          placeholder="password"
+          value={input["password"]}
+          onChange={(event) => changeHandler(event)}
+        />
 
-        <input
-          type="submit"
-          value="login" />
+        <input type="submit" value="login" />
       </form>
-
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({
-  ...state.profile
-})
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => ({
+  ...state.profile,
+});
+const mapDispatchToProps = (dispatch) => {
   return {
-    login: data => dispatch(handleLogin(data))
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+    login: (data) => dispatch(handleLogin(data)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
