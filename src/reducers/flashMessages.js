2c9611b39d0af5b4d74c90419291b88156ea3795
SVRourke <samuelvanrourke@gmail.com>
2021-07-06 08:18:34 -0400

    stubbed out reducer for flash messges

---
+++ src/reducers/flashMessages.js
@@ -0,0 +1,23 @@
+const ADD = "ADDMESSAGE";
+const REMOVE = "REMOVEMESSAGE";
+const CLEAR = "CLEARMESSAGES";
+
+const FlashMessagesReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+    case REMOVE:
+      return state.filter((r) => r.id !== action.id);
+    case CLEAR:
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default FlashMessagesReducer;
+
+// message = {
+//     text: 'Invalid credentials try again',
+//     id: 3
+// }
