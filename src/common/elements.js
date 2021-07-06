dd2b2ceda04850cac183fde96878e1fba0a7f390
SVRourke <samuelvanrourke@gmail.com>
2021-07-06 08:20:32 -0400

    moved elements from globalElements

--- src/common/elements.js
+++ src/common/elements.js
@@ -65,6 +65,43 @@ const NavLink = styled(Link)`
   }
 `;
 
+const FancyLink = styled.div`
+  display: block;
+  margin: 1rem 0;
+  a {
+    border-radius: 3rem;
+    text-decoration: none;
+    border: solid black 2px;
+    padding: 0.5rem 2rem;
+    margin: 2rem 0;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  font-size: 1.3rem;
+
+  & :not(:first-child) {
+    margin-left: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background: none;
+  font-size: inherit;
+  font-weight: ${(props) => (props.active ? "600" : "")};
+  text-decoration: ${(props) => (props.active ? "underline" : "none")};
+  color: ${(props) => (props.active ? "#000" : "#999")};
+  text-decoration-thickness: ${(props) => (props.active ? "2px" : "0")};
+`;
+
+const Heading = styled.p`
+  font-weight: bold;
+`;
+
 export {
   CardBody,
   MemberCount,
@@ -74,4 +111,8 @@ export {
   DeclineButton,
   NavLinkContainer,
   NavLink,
+  FancyLink,
+  Container,
+  Button,
+  Heading,
 };
