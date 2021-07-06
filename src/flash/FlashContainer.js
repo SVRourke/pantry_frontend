632963f48b50d60f6edcfe2120c6b28025590031
SVRourke <samuelvanrourke@gmail.com>
2021-07-06 11:16:21 -0400

    FlashCard and FlashContainer components for displaying messages to the user

---
+++ src/flash/FlashContainer.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import "./flash.scss";
+import { removeMessage, clearMessages } from "../actions/FlashMessageActions";
+
+const FlashCard = ({ msg, cb }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      cb(msg);
+    }, 10000);
+  }, []);
+
+  return (
+    <div className="messageCard">
+      <p>{msg}</p>
+      <button onClick={() => cb(msg)}>x</button>
+    </div>
+  );
+};
+
+const FlashContainer = ({ messages, clear, remove }) => {
+  const lis = messages.map((msg) => {
+    return <FlashCard msg={msg} cb={remove} />;
+  });
+  return <ul id="flashContainer">{lis}</ul>;
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  remove: (msg) => dispatch(removeMessage(msg)),
+  clear: (msg) => dispatch(clearMessages(msg)),
+});
+
+const mapStateToProps = (state) => ({
+  messages: state.flash,
+});
+export default connect(mapStateToProps, mapDispatchToProps)(FlashContainer);
